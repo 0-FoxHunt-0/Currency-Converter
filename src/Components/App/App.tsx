1fbce1f98559828495a0773853d869a56f98b428
Nate <nethanelstahlberg@gmail.com>
2023-09-12 07:25:06 +0300

    Started state

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
+}
+
+export default App;
